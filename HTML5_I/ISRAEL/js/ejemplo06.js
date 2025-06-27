3a70fe971bc488564d2e7e337ba3bd9407ca442e
HENRY DUQUE <hduqueuneweb@gmail.com>
2025-06-27 11:21:13 -0400

    clase 5 HTML5 nivel i

---
+++ HTML5_I/ISRAEL/js/ejemplo06.js
@@ -0,0 +1,12 @@
+function cambiar(){
+          var elemento = document.getElementById('clave');
+          var imagen = document.getElementById('ojo')
+          console.log(elemento.value);
+            if (elemento.type == 'password'){
+                elemento.type='text';
+                imagen.src='../img/nover.png';
+          }else{
+            elemento.type='password';
+            imagen.src='../img/ver.png';
+          };
+    }
